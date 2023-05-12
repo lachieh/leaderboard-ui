faf3fe1755880d080f0cb16cada59381c1bf0dce
Lachlan Heywood <lachieh@users.noreply.github.com>
2023-05-12 10:40:54 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Clickability</h1>
+      </header>
+      <main>
+        TODO: leaderboard
+      </main>
+      <footer>
+        Made with ❤️ by <a href="https://cosmonic.com">Cosmonic</a>
+      </footer>
+    </>
+  );
+}
+
+export default App;
